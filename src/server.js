4a59d87507a482e4f81bc28d4b44067bc4ee740d
hanna_tarasenko <en.anya.tarasenko@gmail.com>
2025-03-31 14:20:44 +0300

    api removed

--- src/server.js
+++ src/server.js
@@ -16,7 +16,7 @@ export const setupServer = () => {
       },
     }),
   );
-  app.get('/api/contacts', async (req, res) => {
+  app.get('/contacts', async (req, res) => {
     const data = await getContacts();
     res.json({
       status: 200,
@@ -25,7 +25,7 @@ export const setupServer = () => {
     });
   });
 
-  app.get('/api/contacts/:id', async (req, res) => {
+  app.get('/contacts/:id', async (req, res) => {
     const { id } = req.params;
     const data = await getContactbyId(id);
 
